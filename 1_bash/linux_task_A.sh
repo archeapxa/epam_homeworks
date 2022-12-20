62f695e05df5d53abd1f695f9b9ed8c9ac20ccd9
Stanislav Iushkevich <stanislav.iushkevich@gmail.com>
2022-12-20 12:59:20 +0200

    added help for script

--- 1_bash/linux_task_A.sh
+++ 1_bash/linux_task_A.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-default_show_help () { echo "show help"; }
+default_show_help () { 
+  echo 'Choose parameter'
+	echo '--all	key - displays the IP addresses and symbolic names(if any) of all hosts in the current subnet'
+	echo '--target key - displays a list of open system TCP ports'
+  echo 'Scans needs nmap and netstat and nmap tools to be installed'
+}
 
 show_hosts () { 
   nmap -sn -oG temp 192.168.0.0/24 >> /dev/null
