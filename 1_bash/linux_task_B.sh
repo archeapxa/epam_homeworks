87d758684f7bb15e44706c19ecdc25c46e6c9e14
archeapxa <stanislav.iushkevich@gmail.com>
2022-12-20 17:31:34 +0200

    b2 and b3 done

--- 1_bash/linux_task_B.sh
+++ 1_bash/linux_task_B.sh
@@ -9,3 +9,16 @@ most_ip=`echo $most_ip_str | cut -d' ' -f2`
 
 echo There were $most_number requests from $most_ip
 
+# 2. Second question
+echo '2. What is the most requested page?'
+
+most_page_str=`cat $1 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort | uniq -c | sort -n | tail -n 1`
+
+echo $most_page_str | cut -d' ' -f2 
+
+
+# 3. Third question
+echo '3. How many requests were there from each ip?'
+
+echo 'Request IP'
+cut -d' ' -f1 $1 | sort | uniq -c | sort -k 1 -n -r
