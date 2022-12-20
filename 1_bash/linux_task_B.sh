99fddad131b0f95f178a7d5fcaa02678bb12e122
archeapxa <stanislav.iushkevich@gmail.com>
2022-12-20 18:42:14 +0200

    fixed 2b, 4b done

--- 1_bash/linux_task_B.sh
+++ 1_bash/linux_task_B.sh
@@ -12,7 +12,7 @@ echo There were $most_number requests from $most_ip
 # 2. Second question
 echo '2. What is the most requested page?'
 
-most_page_str=`cat $1 | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | sort | uniq -c | sort -n | tail -n 1`
+most_page_str=`cat $1 | grep -Eo "/[a-zA-Z0-9./?=_%:-]*.html" | cut -d' ' -f7 | sort | uniq -c | sort -n | tail -n 1`
 
 echo $most_page_str | cut -d' ' -f2 
 
@@ -22,3 +22,10 @@ echo '3. How many requests were there from each ip?'
 
 echo 'Request IP'
 cut -d' ' -f1 $1 | sort | uniq -c | sort -k 1 -n -r
+
+
+
+# 4. Fourth question
+echo '4. What non-existent pages were clients referred to?' 
+
+cat $1 | grep -w '404' | cut -d' ' -f7 |  grep -Eo "/[a-zA-Z0-9./?=_%:-]*.html[a-zA-Z0-9./?=_%:-]*"  | sort | uniq 
